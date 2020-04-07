26553baac5d82cfd51c5727c3537b9f3722021c4
Rodrigo Azevedo <rodrigowbazevedo@yahoo.com.br>
2020-04-07 18:45:10 -0300

    observer-component 0.0.5

--- projects/rodrigowba/observer-component/src/lib/scroll-pagination.component.ts
+++ projects/rodrigowba/observer-component/src/lib/scroll-pagination.component.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable, merge, Subject } from 'rxjs';
 import { distinctUntilChanged, switchMap, throttleTime } from 'rxjs/operators';
 
 import { ObserverComponent } from './observer.component';
-import { Pagination, isInitialPagination, filterPaginationScrollPosition } from '@rodrigowba/http-common';
+import { Pagination, Filters, isInitialPagination, filterPaginationScrollPosition } from '@rodrigowba/http-common';
 
 export abstract class ScrollPaginationComponent extends ObserverComponent implements OnInit {
     private scrollIndex$: Subject<number>;
@@ -16,7 +16,7 @@ export abstract class ScrollPaginationComponent extends ObserverComponent implem
         this.scrollIndex$.next(index);
     }
 
-    selectToLoadPage<T>(pagination$: Observable<Pagination>): Observable<T> {
+    selectToLoadPage<T extends Filters>(pagination$: Observable<Pagination<T>>): Observable<T> {
         return merge<T>(
             pagination$.pipe(
                 isInitialPagination<T>(),
