afcada1ae86452aacf1a062b16fed04e37555fe6
Rodrigo Azevedo <rodrigowbazevedo@yahoo.com.br>
2020-05-14 01:33:12 -0300

    pipes@0.0.3

--- projects/rodrigowba/pipes/src/lib/pipes/is-in.pipe.ts
+++ projects/rodrigowba/pipes/src/lib/pipes/is-in.pipe.ts
@@ -3,6 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'isIn', pure: true })
 export class IsInPipe implements PipeTransform  {
     transform(value: any, array: any[]): boolean {
+        if (!Array.isArray(array)) {
+            return false;
+        }
+
         return array.indexOf(value) !== -1;
     }
 }
