1bdd2f321c4ad77005aa41c3a68515520d4db6ab
Rodrigo Azevedo <rodrigo@rwba.dev>
2022-12-16 13:20:15 -0300

    Fix ofType Typing

--- projects/rodrigowba/ngrx-utils/src/lib/mixins/ofType.mixins.ts
+++ projects/rodrigowba/ngrx-utils/src/lib/mixins/ofType.mixins.ts
@@ -5,9 +5,11 @@ import { Observable } from 'rxjs';
 
 type Constructor<T> = new(...args: any[]) => T;
 
+type UnionTouple<T extends readonly any[]> = T[number];
+
 export interface OfTypeInterface {
   actions$: Actions;
-  ofType<U extends ActionCreator = ActionCreator>(...types: U[]): Observable<ReturnType<U>>;
+  ofType<U extends readonly ActionCreator[]>(...types: U): Observable<ReturnType<UnionTouple<U>>>;
 }
 
 // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
@@ -15,7 +17,7 @@ export function ofTypeMixin<T extends Constructor<{}>>(Base: T = (class {} as an
   return class extends Base implements OfTypeInterface {
     actions$: Actions;
 
-    ofType<U extends ActionCreator = ActionCreator>(...types: U[]): Observable<ReturnType<U>> {
+    ofType<U extends readonly ActionCreator[]>(...types: U): Observable<ReturnType<UnionTouple<U>>> {
       return this.actions$.pipe(
         ofType(...types)
       );
